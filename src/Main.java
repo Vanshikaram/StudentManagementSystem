02f6aab9087dbd7d78b1ad67eff91ec7a07431b0
Vanshika Karamchandani <vanshika.24bce11281@vitbhopal.ac.in>
2026-07-18 23:20:47 +0530

    Implemented StudentManager with add and display student features

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        StudentManager manager = new StudentManager();
+
         Student student1 = new Student(
                 101,
                 "Rahul Sharma",
@@ -10,7 +12,17 @@ public class Main {
                 92.5
         );
 
-        student1.displayStudent();
+        Student student2 = new Student(
+                102,
+                "Priya Singh",
+                19,
+                "Information Technology",
+                89.8
+        );
 
+        manager.addStudent(student1);
+        manager.addStudent(student2);
+
+        manager.displayAllStudents();
     }
 }
---
+++ src/StudentManager.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class StudentManager {
+
+    // List to store students
+    private ArrayList<Student> students;
+
+    // Constructor
+    public StudentManager() {
+        students = new ArrayList<>();
+    }
+
+    // Add a student
+    public void addStudent(Student student) {
+        students.add(student);
+        System.out.println(student.getName() + " added successfully!");
+    }
+
+    // Display all students
+    public void displayAllStudents() {
+
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+            return;
+        }
+
+        System.out.println("\n===== Student List =====");
+
+        for (Student student : students) {
+            student.displayStudent();
+        }
+    }
+}
